e1315c42555244c59ce01903e942c81b1c32c6b2
AmitGupta000 <117368192+AmitGupta000@users.noreply.github.com>
2024-05-06 17:37:40 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed byulty wires.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+        },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "AMD created the first consumer 64-bit processor.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "First-generation computers were based on the integrated circuit (IC) or chip.",
+        "correct_answer": "False",
+        "incorrect_answers":[
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Notebooks are usually just as powerful as a desktop PC.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "A laptop computer is a portable version of a PC.",
+        "correct_answer": "True",
+        "incorrect_answers":[
+            "False"
+        ]
+    },
+]
